c4b039230127bf296f10146e45f66a6102fa0585
edison9556 <ediintel92@gmail.com>
2025-12-29 16:16:23 +0530

    1 Step Master

---
+++ src/test/java/org/base/Demo.java
@@ -0,0 +1,24 @@
+package org.base;
+
+public class Demo {
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		System.out.println("Git 1st update");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
